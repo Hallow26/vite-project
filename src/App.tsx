34078fb48ccf09d3b32eee6e1b633a1d815c920f
Hallow26 <baluyutlawrence.26@gmail.com>
2025-02-10 10:39:33 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Lawrence Manuel Baluyut</h1>
+      <div className="box">
+        <p className="text">CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
